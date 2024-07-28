81831aff62582114337cc29c55326cb4355c8e91
Emmanuel Tamamyan <eremia.tamamyan.04@mail.ru>
2024-07-28 14:15:28 +0400

    Tagging project

---
+++ taging/taging.py
@@ -0,0 +1,23 @@
+import os
+import eyed3
+import mutagen.mp4
+import translit
+import mutagen
+
+Directory = 'D:\\matlog'
+
+def tagging(Directory : str):
+    for root, directory, filenames in os.walk(Directory):
+        for names in filenames:
+            set_tags(os.path.join(root,names))
+            
+def set_tags(filename : str):
+    if filename.endswith('.mp4'):
+        mp = mutagen.mp4.MP4(filename)
+        mp.tags['\xa9nam'] = 'Yourname'
+        mp.tags['\xa9ART'] = 'Artist'
+        mp.tags['\xa9alb'] = 'favorite album'
+        mp.tags['\xa9day'] = '?????'
+        mp.save()
+    print(__name__)
+    tagging(Directory)
